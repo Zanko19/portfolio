d1b65e8a03063848308cb83f0ff4d23cdd882548
Guillaume <guizanko@hotmail.fr>
2024-01-22 19:50:22 +0100

    stoptoday

--- src/components/navleft.jsx
+++ src/components/navleft.jsx
@@ -4,7 +4,7 @@ import "../App.css";
 function navleft() {
   return (
     <>
-      <div className="border border-black absolute h-screen flex flex-col lg:items-start items-center lg:ml-10 justify-start">
+      <div className="border border-black absolute h-screen flex flex-col items-center justify-start ">
         <div className="text-3xl text-center">
             navleft
             </div>
