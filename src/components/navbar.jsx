81905eff33409f41663816059a3b98d89fe7adda
Guillaume <guizanko@hotmail.fr>
2024-02-27 23:50:01 +0100

    0

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,6 +6,7 @@ function navbar() {
     <>
       <div className="border border-black">
         <div className="text-3xl text-center">
+        <button className="btn glass">Info</button>
             navbarrr
             </div>
       </div>
