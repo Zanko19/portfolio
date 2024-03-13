327674d109e2a600724f38587ac5d5c1b3ffba0d
Guillaume <guizanko@hotmail.fr>
2024-03-13 12:33:09 +0100

    ok

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,19 @@ import "../App.css";
 import Navbar from "./navbar"; // Assurez-vous que le chemin du fichier navbar est correct
 import Footer from "./footer";
 import Navleft from "./navleft"
+import Button from "./button";
+import FormInput from "./Form";
+import AppBanner from "./banner";
 
 function App() {
   // Si vous avez besoin de useState, assurez-vous de l'utiliser ici
 
   return (
     <>
+    <AppBanner />
       <Navbar />
+      <Button />
+      <FormInput />
       <Navleft />
       <Footer />
     </>
