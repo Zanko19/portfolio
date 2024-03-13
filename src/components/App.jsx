c6cead4af0628338ad4b0891de4e9bcb376918ef
Guillaume <guizanko@hotmail.fr>
2024-03-13 16:19:59 +0100

    çavance

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,22 @@
 import React from "react";
 import { useState } from "react";
 import "../App.css";
-import Navbar from "./navbar"; // Assurez-vous que le chemin du fichier navbar est correct
 import Footer from "./footer";
-import Navleft from "./navleft"
-import Button from "./button";
-import FormInput from "./Form";
-import AppBanner from "./banner";
+
+import Navbar from "./Nav";
+
 
 function App() {
   // Si vous avez besoin de useState, assurez-vous de l'utiliser ici
 
   return (
     <>
-    <AppBanner />
       <Navbar />
-      <Button />
-      <FormInput />
-      <Navleft />
+      
+  
+      
       <Footer />
+
     </>
   );
 }
